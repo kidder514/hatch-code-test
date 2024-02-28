9b39018f4cbe13e44b3510833086a48a431c5daf
Lu <kidder5142@gmail.com>
2024-02-28 17:02:10 +1100

    able to select division

--- src/component/chart/Chart.tsx
+++ src/component/chart/Chart.tsx
@@ -1,9 +1,12 @@
+import { useAtom } from "jotai";
 import { useLoadDivisionList } from "../../api/query";
 import Loader from "../loader";
 import ChartNode from "./ChartNode";
+import { selectDivisionListAtom } from "../../store";
 
 const Chart = () => {
     const { isLoading, data } = useLoadDivisionList();
+    const [selectedDivisionList, setSelectedDivision] = useAtom(selectDivisionListAtom);
 
     if (isLoading) {
         return <Loader />
@@ -11,7 +14,7 @@ const Chart = () => {
 
     const isListEmpty = !data || data.length <= 0;
     return <section className={'mt-4'}>
-        {(!isListEmpty) && data.map((division: any) => <ChartNode className='root' key={`root-node-${division.id}`} data={division} />)}
+        {(!isListEmpty) && data.map((division: any) => <ChartNode selectedDivisionList={selectedDivisionList} setSelectedDivision={setSelectedDivision} className='root' key={`root-node-${division.id}`} data={division} />)}
     </section>
 }
 
--- src/component/chart/ChartNode.tsx
+++ src/component/chart/ChartNode.tsx
@@ -2,38 +2,64 @@ import { Button } from "react-bootstrap";
 import { isEmpty } from "../../util/object";
 
 import './ChartNode.scss'
-import { useState } from "react";
 
 interface ChartNodeProps {
-    data: DivisionNode,
+    data: DivisionNode;
+    selectedDivisionList: number[];
+    setSelectedDivision?: any;
     className?: string;
 }
 
-
-
-const ChartNode = ({ data, className }: ChartNodeProps) => {
-    const [isExpanded, setIsExpanded] = useState(true);
-
+const ChartNode = ({ data, selectedDivisionList, setSelectedDivision, className }: ChartNodeProps) => {
     const renderChildren = (children: { [key: number]: DivisionNode }) => {
         if (isEmpty(children)) return;
         return Object
             .entries(children)
-            .map(([key, value]) => < ChartNode key={`node-${value.id}`} data={value} />);
+            .map(([key, value]) => {
+                return (
+                    <ChartNode
+                        selectedDivisionList={selectedDivisionList}
+                        setSelectedDivision={setSelectedDivision}
+                        key={`node-${value.id}`}
+                        data={value}
+                    />)
+            });
+    }
+
+    const selectHandler = () => {
+        let listSelected;
+        if (selectedDivisionList.includes(data.id)) {
+            listSelected = selectedDivisionList.filter(id => id !== data.id);
+        } else {
+            listSelected = [...selectedDivisionList];
+            listSelected.push(data.id);
+        }
+
+        setSelectedDivision(listSelected);
     }
 
     return (
         <section className={`division-node ${className || ''}`} style={{
             paddingLeft: `${1.5 * (data.level - 1)}rem`
         }}>
-            <div className='parent-wrapper' onClick={() => setIsExpanded(!isExpanded)}>
-                <div className='title'>{isExpanded ? '-' : '+'} {data.name}</div>
-                <div className='button-wrapper'>
-                    <Button variant="primary" size='sm'>+ Division</Button>
-                    <Button variant="primary" size='sm'>+ Entity</Button>
+            <div
+                className={`parent-wrapper ${selectedDivisionList?.includes(data.id) ? 'selected' : ''}`}
+                onClick={selectHandler}
+            >
+                <div className='title'>{data.name}</div>
+                <div className='action-wrapper'>
+                    <div className='button-wrapper'>
+                        <Button variant="primary" size='sm'>+ Division</Button>
+                        <Button variant="primary" size='sm'>+ Entity</Button>
+                    </div>
+                    <div className='form-wrapper'>
+                        {/* <AddDivisionForm /> */}
+                        {/* <AddEntityForm /> */}
+                    </div>
                 </div>
             </div>
 
-            {isExpanded && renderChildren(data.children)}
+            {renderChildren(data.children)}
         </section >
     )
 }
